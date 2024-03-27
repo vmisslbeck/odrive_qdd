12c13360e8cb9394d492c1097cafccbd2a484c70
Valentin Misslbeck <v.misslbeck@hm.edu>
2024-03-27 17:37:47 +0100

    add keyboardinterrupt to usb demo

--- src/odrive_usb_demo.py
+++ src/odrive_usb_demo.py
@@ -21,17 +21,12 @@ def calibrate_motor(motor):
 
 # Find a connected ODrive (this will block until you connect one)
 print("finding an odrive...")
-print(odrive.connected_devices)
-print(odrive.find_any())
+#print(odrive.connected_devices)
+print(type(odrive.find_any()))
 my_drive = odrive.find_any()
-print(odrive.find_any())
 print("Odrive found!")
-print(odrive.connected_devices)
+#print(odrive.connected_devices)
 my_drive.clear_errors()
-
-# Calibrate motor and wait for it to finish
-
-
 
 # To read a value, simply read the property
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
@@ -41,26 +36,28 @@ my_drive.axis0.controller.input_pos = 3.14
 print("Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
 
 # And this is how function calls are done:
-for i in [1,2,3,4]:
-    print('voltage on GPIO{} is {} Volt'.format(i, my_drive.get_adc_voltage(i)))
+# for i in [1,2,3,4]:
+#    print('voltage on GPIO{} is {} Volt'.format(i, my_drive.get_adc_voltage(i)))
 
-calibrate_motor(my_drive.axis0)
-my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+# Calibrate motor and wait for it to finish
+# calibrate_motor(my_drive.axis0)
+
+my_drive.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
 # A sine wave to test
+print("setpoint: ", my_drive.axis0.controller.pos_setpoint)
+time.sleep(1)
 t0 = time.monotonic()
-while True:
+try:
+    while True:
   
-    SINE_PERIOD = 2.0
-    t = time.monotonic() - t0
-    phase = t * (2 * math.pi / SINE_PERIOD)
-    setpoint = math.sin(phase)
-    my_drive.axis0.controller.input_pos = setpoint
-    time.sleep(0.01)
+        SINE_PERIOD = 2.0
+        t = time.monotonic() - t0
+        phase = t * (2 * math.pi / SINE_PERIOD)
+        setpoint = math.sin(phase)
+        my_drive.axis0.controller.input_pos = setpoint
+        time.sleep(0.01)
 
-# Some more things you can try:
-
-# Write to a read-only property:
-my_drive.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
-
-# Assign an incompatible value:
-my_drive.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
+except KeyboardInterrupt:
+    my_drive.axis0.requested_state = AxisState.IDLE
+    print("Motor stopped.")
+    pass
