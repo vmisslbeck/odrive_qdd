5ede5c5d6eaffc3a15d70950f3bfb7d4277a9aaa
Valentin Misslbeck <v.misslbeck@hm.edu>
2024-04-08 18:00:11 +0200

    add dead_zone_guard method

--- src/odrv_movements.py
+++ src/odrv_movements.py
@@ -8,13 +8,20 @@ import math
 
 class position_movements:
     ''' for the movements class, the odrive has to be '''
-    def __init__(self, odrv, gear_ratio_xto1: float = 1):
+    def __init__(self, odrv, gear_ratio_xto1: float = 1, circular_sector: list = [0, 360]):
+        '''
+        about circular_sector: if your motor should only move in a certain sector, you can define it here.
+        if it can move 360 degrees, you can leave it as it is. If you want to move it only from 0 to 180 degrees,
+        then the motor will never be in the sector from 180.1 to 360 degrees.
+        You may wonder if the encoder position will be the same, when odrive is powered off and on again.
+        '''
         self.odrv = odrv
         self.gear_ratio_xto1 = gear_ratio_xto1
+        self.circular_sector = circular_sector
 
     def _set_ctrl_mode(self, mode: ControlMode = ControlMode.POSITION_CONTROL):
         ''' 
-        Set the control mode.
+        Set the control mode. Mandatory for every movement method.
         '''
         self.odrv.axis0.controller.config.control_mode = mode
 
@@ -25,6 +32,34 @@ class position_movements:
         if self.odrv.axis0.disarm_reason != 0:
             print("Motor disarmed. Reason: " + str(self.odrv.axis0.disarm_reason))
             raise KeyboardInterrupt
+        
+    def dead_zone_guard(self, set_position: float):
+        ''' 
+        Guard the dead zone. The dead zone is the sector where the motor should not move.
+        '''
+        if self.circular_sector == [0, 360]:
+            pass
+        absolute_is_pos = float(self.odrv.axis0.controller.pos_setpoint)
+        relative_is_pos = absolute_is_pos % 1
+
+        absolute_set_pos = set_position
+        relative_set_pos = absolute_set_pos % 1
+
+        delta_pos = absolute_set_pos - absolute_is_pos
+
+        if self.circular_sector[0] <= relative_set_pos*360 <= self.circular_sector[1]: #
+            return set_position
+        else:
+            return self.odrv.axis0.encoder.pos_estimates
+        
+    def move_to_zero(self):
+        ''' 
+        Move to encoder position 0.
+        '''
+        self._set_ctrl_mode()
+        self.odrv.axis0.controller.input_pos = 0
+
+        self.disarm_interrupt()
 
     def move_to_position(self, position: float):
         ''' 
@@ -38,12 +73,13 @@ class position_movements:
     def move_to_angle(self, angle: float):
         ''' 
         Move to a specific angle. Note that angle can go from 0 to 360 degrees.
-        For example, if the motor is in encoder position 5000 and you want to move to angle 0, the motor won't move t0 encoder position 0, 
-        but the nearest encoder position to angle 0.
+        For example, if the motor is in encoder position 5000 and you want to move to angle 0, 
+        the motor won't move to encoder position 0, but the nearest encoder position to angle 0.
         '''
         self._set_ctrl_mode()
         pos = float(self.odrv.axis0.controller.pos_setpoint)
-        pos += angle * 8192 / 360 * self.gear_ratio_xto1
+        # pos += angle * 8192 / 360 * self.gear_ratio_xto1
+        pos += angle /360 * self.gear_ratio_xto1
         self.odrv.axis0.controller.input_pos = pos
         #what is the 8192? it is the number of encoder counts per revolution
         #how do we know that? we can check it in the odrivetool by typing odrv0.axis0.encoder.config
