f4d56b4f8ff25e7c98455fc81a78b5d165b759cc
Valentin Misslbeck <v.misslbeck@hm.edu>
2024-07-27 10:30:21 +0200

    add classes to odrv_plot.py

--- src/odrv_plot.py
+++ src/odrv_plot.py
@@ -3,41 +3,40 @@ import matplotlib.animation as animation
 import odrv_con_and_calib as concal
 import odrv_movements as mvm
 
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
+class PlotMe:
+    def __init__(self, odrv):
+        self.odrv = odrv
+        self.mvmts = mvm.BaseMovements(odrv)
 
-# Initialize odrive
+    def plot_position(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        xs = []
+        ys = []
+
+        def animate(i, xs, ys):
+            pos = round(self.mvmts.get_rel_pos(), 5)
+
+            xs.append(len(xs))
+            ys.append(pos)
+
+            xs = xs[-1000:]
+            ys = ys[-1000:]
+
+            ax.clear()
+            ax.plot(xs, ys)
+
+            plt.xticks(rotation=45, ha='right')
+            plt.subplots_adjust(bottom=0.30)
+            plt.title('Rotational Position over Time')
+            plt.ylabel('turns')
+            plt.xlabel('time')
+
+        ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=10, cache_frame_data=False)
+        plt.show()
+
+
 cc = concal.Utils()
 my_drive = cc.find_one_odrive()
-mvmts = mvm.BaseMovements(my_drive)
 
-def animate(i, xs, ys):
-
-
-    pos = round(mvmts.get_rel_pos(), 5)
-
-    # Add x and y to lists
-    xs.append(len(xs))
-    ys.append(pos)
-
-    # Limit x and y lists to 1000 items #choose how many values you want to see
-    xs = xs[-1000:]
-    ys = ys[-1000:]
-
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Rotational Position over Time')
-    plt.ylabel('turns')
-    plt.xlabel('time')
-
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=10, cache_frame_data=False)
-plt.show()
+PlotMe(my_drive).plot_position()
